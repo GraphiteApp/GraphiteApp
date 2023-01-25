d86b106618087c9743c2f319bfbe79137e7e30f2
TheArchons <progamer9138@gmail.com>
2023-01-25 11:12:28 -0800

    remove exam activity

--- app/src/main/java/com/example/classmonitor/MainActivity.kt
+++ app/src/main/java/com/example/classmonitor/MainActivity.kt
@@ -54,13 +54,6 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
-        // onclick exam, move to exam activity
-        menu.findItem(R.id.action_exam).setOnMenuItemClickListener {
-            val intent = android.content.Intent(this, ExamActivity::class.java)
-            startActivity(intent)
-            true
-        }
-
 
         return true
     }
