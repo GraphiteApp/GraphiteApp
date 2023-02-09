b2e1f652a40c0b82575dbbf3eeb6e07a0fcfd3dd
TheArchons <progamer9138@gmail.com>
2023-02-08 16:27:55 -0800

    move screen sharing to another branch

--- app/src/main/java/com/example/graphite/ExamActivity.kt
+++ app/src/main/java/com/example/graphite/ExamActivity.kt
@@ -20,9 +20,6 @@ class ExamActivity : AppCompatActivity() {
         val app = MainActivity.app
     }
 
-    // media recorder, null by default
-    var mMediaRecorder: MediaRecorder? = null
-
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,54 +31,6 @@ class ExamActivity : AppCompatActivity() {
         app.setIsExamMode(true)
 
         setContentView(R.layout.activity_exam)
-
-        // request permission to record screen
-        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
-
-        // start video recording
-        recorder(true)
-    }
-
-    @RequiresApi(Build.VERSION_CODES.S)
-    private fun recorder(isStart: Boolean) {
-        if (isStart) {
-            // start video recording
-
-            // create media recorder
-            mMediaRecorder = MediaRecorder(this)
-
-            // get media projection manager
-            getSystemService(MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
-            val width = Resources.getSystem().displayMetrics.widthPixels
-            val height = Resources.getSystem().displayMetrics.heightPixels
-
-            // print width and height
-            println("Width: $width")
-            println("Height: $height")
-
-            mMediaRecorder!!.setVideoSource(MediaRecorder.VideoSource.SURFACE)
-
-            mMediaRecorder!!.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
-
-            mMediaRecorder!!.setVideoEncoder(MediaRecorder.VideoEncoder.H264)
-
-            mMediaRecorder!!.setVideoFrameRate(15)
-
-            mMediaRecorder!!.setVideoSize(width, height)
-
-            // get media path
-            val mediaPath = getExternalFilesDir(null)?.absolutePath
-
-            mMediaRecorder!!.setOutputFile("$mediaPath" + "test.m4e")
-
-            println("Media path: $mediaPath")
-
-            mMediaRecorder?.prepare()
-
-            mMediaRecorder?.start()
-        } else {
-            //mMediaRecorder?.stop()
-        }
     }
 
     @RequiresApi(Build.VERSION_CODES.S)
@@ -90,9 +39,6 @@ class ExamActivity : AppCompatActivity() {
         val teacherCode = sharedPref.getString("teacher_code", "").toString().uppercase()
         val username = sharedPref.getString("pref_key_name", "").toString()
         val apiURL = sharedPref.getString("api_url", "").toString()
-
-        // stop video recording
-        recorder(false)
 
         // send POST request to server
         val client = OkHttpClient()
